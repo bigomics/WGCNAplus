316a82a9aacafaaed57356bf4b526c87f0a79474
zitoa <zito.a@hotmail.com>
2026-02-23 17:54:11 +0100

    polish vignette

--- vignettes/test-consensus.R
+++ vignettes/test-consensus.R
@@ -17,21 +17,18 @@ group <- pgx$samples$sex
 xx <- tapply(1:ncol(pgx$X), group, function(ii) pgx$X[,ii])
 names(xx)
 
-par(mfrow=c(2,3), mar=c(5,5,3,1), cex=1.4)
-plotPowerAnalysis( t(xx[[1]]), setPar=FALSE)
-plotPowerAnalysis( t(xx[[2]]), setPar=FALSE)
+par(mfrow = c(2, 3), mar = c(5, 5, 3, 1), cex = 1.4)
+plotPowerAnalysis(t(xx[[1]]), setPar = FALSE)
+plotPowerAnalysis(t(xx[[2]]), setPar = FALSE)
 
 ## This runs consensus WGCNA on an expression list
 names(xx)
 Y <- pgx$samples[,3:6]
-##Y <- expandPhenoMatrix(Y)
-head(Y)
 
 cons <- runConsensusWGCNA(
   xx,
   phenoData = Y,
   power = 6,
-  #GMT = pgx$GMT,
   annot = pgx$genes,
   compute.enrichment = 1,
   ngenes = 2000,
@@ -52,21 +49,25 @@ names(cons)
 cons$net$power
 
 x11()
-par(cex=1.4)
+par(cex = 1.4)
 plotDendroAndColors(
-  cons, marAll=c(2,10,3,1), show.traits=1, show.kme=0, use.tree=0,
-  colorHeight = 0.2, colorHeightMax = 0.7, setLayout=1
-)  
+  cons,
+  marAll = c(2, 10, 3, 1),
+  show.traits = 1,
+  show.kme = 0,
+  use.tree = 0,
+  colorHeight = 0.2,
+  colorHeightMax = 0.7,
+  setLayout = 1
+)
 
 head(cons$datTraits)
-plotModuleScores(cons, trait="ab_fat", nmax = 9)
-plotModuleScores(cons$layers[[1]], trait="ab_fat", nmax = 9)
+plotModuleScores(cons, trait = "ab_fat", nmax = 9)
+plotModuleScores(cons$layers[[1]], trait = "ab_fat", nmax = 9)
 
-plotConsensusTraitCorrelation(cons, traits=NULL) 
+plotConsensusTraitCorrelation(cons, traits = NULL) 
 
-##--------------------------------------------------------------------
 ## gene statistics
-##--------------------------------------------------------------------
 load_all()
 
 top <- getTopGenesAndSets(cons, module=NULL, ntop=10) 
@@ -87,17 +88,16 @@ names(stats2)
 head(stats2[['full']])
 head(stats2[['consensus']])
 
-##--------------------------------------
 ## enrichment
-##--------------------------------------
-
 GMT = Matrix::t(playdata::GSETxGENE)
 annot = pgx$genes
 cons$gsea <- computeConsensusModuleEnrichment(
-  cons, GMT=GMT, annot=annot,
+  cons,
+  GMT = GMT,
+  annot = annot,
   methods = c("fisher","gsetcor","xcor"),
-  #methods = c("xcor"),
-  min.genes=5, ntop=1000
+  min.genes = 5,
+  ntop = 1000
 )
 
 names(cons$gsea)
@@ -109,10 +109,7 @@ ai_model = "groq:openai/gpt-oss-20b"
 playbase::ai.genesets_summary(top.gs, model=ai_model)
 playbase::ai.genesets_keywords(top.gs, model=ai_model) 
 
-##------------------------------------------------------------
 ## Sample clustering dendrograms (useless?)
-##------------------------------------------------------------
-
 lapply(xx, dim)
 dim(cons$modTraits)
 head(cons$datTraits)[,1:4]
@@ -120,11 +117,11 @@ head(cons$datTraits)[,1:4]
 nsets <- length(xx)
 layout.matrix <- matrix( 1:(2*nsets), nrow = 2, ncol = nsets)
 layout(layout.matrix, heights=c(1,1), widths=rep(1,nsets))
-i=1
 for(i in 1:nsets) {
   dt <- toupper(names(cons$datExpr)[i])
   plotConsensusSampleDendroAndColors(
-    cons, i,
+    cons,
+    i,
     main = paste("sample tree and traits heatmap for", dt),
     what = c("me", "traits", "both")[2],
     marAll = c(0.2, 8, 1, 0.2),
@@ -134,10 +131,7 @@ for(i in 1:nsets) {
   ) 
 }
 
-##------------------------------------------------------------
 ## Module-trait heatmaps (important plots)
-##------------------------------------------------------------
-
 lapply(cons$zlist, dim)
 ii <- hclust(dist(cons$zlist[[1]]))$order
 jj <- hclust(dist(t(cons$zlist[[1]])))$order
@@ -148,13 +142,19 @@ tZ <- Reduce('+', lapply(lapply(cons$zlist,t),dist))
 ii <- hclust(Z)$order
 jj <- hclust(tZ)$order
 
-par(mfrow=c(2,3), mar=c(8,12,3,1), cex=0.8)
+par(mfrow = c(2, 3), mar = c(8, 12, 3, 1), cex = 0.8)
 for(i in 1:length(cons$zlist)) {
   k <- names(cons$zlist)[i]
   plotLabeledCorrelationHeatmap(
-    cons$zlist[[i]][ii,jj], cons$ydim[i], cex.lab = 1.2,
-    pstar=1, text=FALSE, cluster=FALSE, setpar=FALSE,
-    main = paste("module-trait for",toupper(k)))
+    cons$zlist[[i]][ii,jj],
+    cons$ydim[i],
+    cex.lab = 1.2,
+    pstar = 1,
+    text = FALSE,
+    cluster = FALSE,
+    setpar = FALSE,
+    main = paste("module-trait for",toupper(k))
+  )
 }
 
 ## Consensus Module-Trait
@@ -169,14 +169,18 @@ plotLabeledCorrelationHeatmap(
 )
 
 ## Distinct Module-Trait
-diffZ <- computeDistinctMatrix(
-  matlist, ydim=ydim, psig = 0.05, min.diff=0.1) 
+diffZ <- computeDistinctMatrix(matlist, ydim = ydim, psig = 0.05, min.diff = 0.1) 
 names(diffZ)
 for(set in names(diffZ)) {
   plotLabeledCorrelationHeatmap(
-    diffZ[[set]][ii,jj], nsamples, setpar=FALSE,
-    text=FALSE, pstar=1, cluster=FALSE, cex.lab=1.2,
-    main = paste("unique Module-Traits for",toupper(set))
+    diffZ[[set]][ii,jj],
+    nsamples,
+    setpar = FALSE,
+    text = FALSE,
+    pstar = 1,
+    cluster = FALSE,
+    cex.lab = 1.2,
+    main = paste("unique Module-Traits for", toupper(set))
   )
 }
 
@@ -186,11 +190,15 @@ z0[is.na(z0)] <- 0
 z1 <- diffZ
 for(i in 1:length(z1)) z1[[i]][is.na(z1[[i]])] <- 0
 effZ <- abs(z0) - Reduce('+', lapply(z1,abs))
-effZ
 wgcna.plotLabeledCorrelationHeatmap(
-  effZ[ii,jj], nsamples, setpar=FALSE,
+  effZ[ii,jj],
+  nsamples,
+  setpar = FALSE,
   colorpal = purpleGreyYellow,
-  text=FALSE, pstar=1, cluster=FALSE, cex.lab=1.2,
+  text = FALSE,
+  pstar = 1,
+  cluster = FALSE,
+  cex.lab = 1.2,
   main = "effective consensus"
 )
 
@@ -202,12 +210,16 @@ MEs <- cons$net$multiMEs
 lapply(MEs, function(m) dim(m$data))
 modules <- colnames(MEs[[1]]$data)
 
-par(mfrow=c(2,1), mar=c(12,4,2,2))
+par(mfrow = c(2, 1), mar = c(12, 4, 2, 2))
 for(k in names(MEs)) {
   ME <- MEs[[k]]$data
   plotEigenGeneClusterDendrogram(
-    wgcna=NULL, ME=ME, main = k,
-    setMargins=FALSE, method="hclust")
+    wgcna = NULL,
+    ME = ME,
+    main = k,
+    setMargins = FALSE,
+    method = "hclust"
+  )
 }
 
 par(cex = 0.9)
@@ -215,24 +227,19 @@ WGCNA::plotEigengeneNetworks(
   MEs,
   setLabels = names(xx),
   plotDendrograms = FALSE,
-#  plotHeatmaps = FALSE,
   marDendro = c(0,2,2,1)*2,
   marHeatmap = c(3,3,2,1)*2,
   zlimPreservation = c(0.5, 1),
   xLabelsAngle = 90
 )
 
-x
-#----------------------------------------------------------------
-# Create correspondence table (not so usefull...)
-#----------------------------------------------------------------
 
+# Create correspondence table (not so usefull...)
 net0 <- cons$net
 names(cons$netList)
 net1 <- cons$layers[[1]]$net
 net2 <- cons$layers[[2]]$net 
 names(cons$layers)
-par(cex=1.3)
-plotConsensusOverlapHeatmap(
-  net1, net2, setLabels=names(cons$netList))
+par(cex = 1.3)
+plotConsensusOverlapHeatmap(net1, net2, setLabels=names(cons$netList))
 
